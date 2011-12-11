4510a8525f4fdc0a37aeb9a9302a29fcd10b1b34
Frederic <frederic@askarel.be>
2011-12-11 20:35:54 +0100

    - Added font setting - No longer need data to be piped in: use -t option - TBD: how the sign deal with out-of-order tags ?

--- amoled.sh
+++ amoled.sh
@@ -26,10 +26,13 @@ SERIALPORT="/dev/ttyUSB0"
 WTIME="4"	# Display wait time
 ADDR="1"	# Sign address
 SLINE="1"	# Line on the display
+FONTTAG="7"	# Use default display font
+#SCOLORTAG="20"	# Use default sign color (usually orange)
 
 # Some data. Output tag on the left, text description on the right.
+
 # Available sign effects
-# Tags LMNPQRS are for opening only.
+# Tags LMNPQRS are for opening only. (mandatory)
 SEFFECT="|help,<FA>|immediate,<FB>|xopen,<FC>|curtainup,<FD>|curtaindown,<FE>|scrollleft,<FF>|scrollright,<FG>|vopen"
 SEFFECT="$SEFFECT,<FH>|vclose,<FI>|scrollup,<FJ>|scrolldown,<FK>|hold,<FL>|snow,<FM>|twinkle,<FN>|blockmove"
 SEFFECT="$SEFFECT,<FP>|random,<FQ>|penwritehello,<FR>|penwritewelcome,<FS>|penwriteam"
@@ -39,8 +42,8 @@ LCSEFFECT=12
 # Available sign colors
 SCOLOR="|help,<CA>|dimred,<CB>|red,<CC>|brightred,<CD>|dimgreen,<CE>|green,<CF>|brightgreen,<CG>|dimorange,<CH>|orange"
 SCOLOR="$SCOLOR,<CI>|brightorange,<CJ>|yellow,<CK>|lime,<CL>|inversered,<CM>|inversegreen,<CN>|inverseorange"
-SCOLOR="$SCOLOR,<CP>|redongreen,<CQ>|greenonred,<CR>|redyellowgreen,<CS>|rainbow"
-LSCOLOR=18
+SCOLOR="$SCOLOR,<CP>|redongreen,<CQ>|greenonred,<CR>|redyellowgreen,<CS>|rainbow,|"
+LSCOLOR=20
 
 # font sizes
 # A: 5x7 pixels
@@ -48,14 +51,19 @@ LSCOLOR=18
 # C: 4x7 pixels
 # D: 7x13 pixels, for displays with > 16 pixels height only
 # E: 5x8 pixels, for displays with > 7 pixels height only
-SFONT="|help,<AA>|5x7,<AB>|6x7,<AC>|4x7,<AD>|7x13,<AE>|5x8"
-LSFONT=6
+SFONT="|help,<AA>|5x7,<AB>|6x7,<AC>|4x7,<AD>|7x13,<AE>|5x8,|"
+LSFONT=7
 
 # Wait tags
-# A: 0.5 seconds, Z: 25 seconds.
+# A: 0.5 seconds, Z: 25 seconds. (mandatory)
 SWTIME="|help,<WA>|0.5,<WB>|1,<WC>|2,<WD>|3,<WE>|4,<WF>|5,<WG>|6,<WH>|7,<WI>|8,<WJ>|9,<WK>|10,<WL>|11,<WM>|12,<WN>|13"
 SWTIME="$SWTIME,<WO>|14,<WP>|15,<WQ>|16,<WR>|17,<WS>|18,<WT>|19,<WU>|20,<WV>|21,<WW>|22,<WX>|23,<WY>|24,<WZ>|25"
 LSWTIME=27
+
+# Bell tags, by 0.5 seconds increment (optional)
+SBELL="|help,<BA>|0.5,<BB>|1,<BC>|1.5,<BD>|2,<BE>|2.5,<BF>|3,<BG>|3.5,<BH>|4,<BI>|4.5,<BJ>|5,<BK>|5.5,<BL>|6,<BM>|6.5"
+SBELL="$SBELL,<BN>|7,<BO>|7.5,<BP>|8,<BQ>|8.5,,<BR>|9,<BS>|9.5,<BT>|10,<BU>|10.5,<BV>|11,<BW>|11.5,<BX>|12,<BY>|12.5,<BZ>|13,|"
+LSBELL=28
 
 # Function to call when we bail out
 die ()
@@ -132,12 +140,18 @@ taghelp ()
     exit 1
 }
 
+# Make a text page. Add some color and a font before the text.
+textpage ()
+{
+    echo -n "$(gettag $SCOLOR $SCOLORTAG)$(gettag $SFONT $FONTTAG)$1"
+}
+
 #Parameter list:
 # 1: <Mn> Display method (bitfield ABCDEQRSTUabcdeqrstu) NEED A REWRITE
 # 2: Text data
 makepage ()
 {
-    echo -n "<L$SLINE><P$SPAGE>$(gettag $SEFFECT $INTROTAG)<M$1>$(gettag $SWTIME $WTIME)$(gettag $SEFFECT $OUTTROTAG)$(gettag $SCOLOR $SCOLORTAG)$2"
+    echo -n "<L$SLINE><P$SPAGE>$(gettag $SEFFECT $INTROTAG)<M$1>$(gettag $SWTIME $WTIME)$(gettag $SEFFECT $OUTTROTAG)$2"
 }
 
 linkpages ()
@@ -149,11 +163,12 @@ linkpages ()
 # data checksum and insert end tag. At this point, the data is ready to be 
 # sent to the display.
 preparedataforsign ()
-{
-    XORSCRATCH="$(printf '%d' "'${1:0:1}")"
+{	
+    # Note: in Bash, the string start at zero.
+    XORSCRATCH="$(printf '%d' "'${1:0:1}")" #' Syntax highlighters choke on that.
     for i in $(seq 1 $(($(echo -n "$1" |wc -m )-1))); 
     do 
-	XORSCRATCH=$(( $XORSCRATCH ^ $(printf '%d' "'${1:$i:1}") ))
+	XORSCRATCH=$(( $XORSCRATCH ^ $(printf '%d' "'${1:$i:1}") )) #' Syntax highlighters choke on that.
     done
     if [ "$SERIALPORT" = "-" ]; then
         printf "<ID%.2X>$1%X<E>\n" $ADDR $XORSCRATCH; 
@@ -173,22 +188,29 @@ preparedataforsign ()
 usage ()
 {
  echo "$ME: Small script to update LED signs made by Amplus.
-  Usage: echo \"your text\" | $ME [options]
+  Usage: echo $ME [options]
  
  Sign controls and port settings:
-    -a N	Sign address (0 is broadcast, default address is $ADDR)
-    -d dev	Serial port to use. Default is $SERIALPORT. Use - for stdout.
-    -f		Flush sign memory.
+    -A N	Sign address (0 is broadcast, default address is $ADDR)
+    -D dev	Serial port to use. Default is $SERIALPORT. Use - for stdout.
+    -F		Flush sign memory.
     -h		This help screen
-    -k AB...	Link pages together for immediate display
-    -t		Set the internal clock of the sign using system clock.
+    -K AB...	Link pages together for immediate display
+    -T		Set the internal clock of the sign using system clock.
  Page data:
     -p N	Page number (A-Z) Mandatory parameter.
     -i anim	Define the opening animation of the page. Random intro if not specified. (use -i help for options)
-    -w N	wait time, from 0.5 seconds to 25 seconds. Default is $(gettagtext $SWTIME $WTIME)
+    -w N	wait time, from 0.5 second to 25 seconds. Default is $(gettagtext $SWTIME $WTIME)
     -o anim	Define the closing animation of the page. Random outtro if not specified. (use -o help for options)
     -l N	Line number, default is $SLINE
     -c color	Set text color. Random if not specified (use -c help for options)
+    -f font	Font to use. (Use -f help for options)
+    -s N	Skip N 1-pixel-wide columns from left side
+    -t TEXT	Text to display
+    -g DATA	Graphic block to display (not implemented)
+    -k		Insert clock (not implemented)
+    -d		Insert date (not implemented)
+    -b N	Ring the bell for N seconds (0.5 to 13 seconds with 0.5 seconds increment) (not implemented)
     -R		Treat input data as raw page message data. (not implemented)
 "
  exit 1
@@ -196,23 +218,25 @@ usage ()
 
 parse_arguments ()
 {
-    while getopts ":ha:p:i:w:o:ftRd:l:k:c:" OPTION
+    while getopts ":hA:p:i:w:o:FTRD:l:K:c:f:t:" OPTION
     do
 	case "$OPTION" in
-	    a)	ADDR="$OPTARG"		;;	# OK
+	    A)	ADDR="$OPTARG"		;;	# OK
+	    D)	SERIALPORT="$OPTARG"	;;	# OK
+	    F)	SCOMMAND="resetsign"	;;	# OK
+	    K)	LINKPAGES="$(echo "$OPTARG" | tr '[:lower:]' '[:upper:]')"
+		SCOMMAND="linkpages"
+		;;	# OK
+	    T)	SCOMMAND="setsignclock"	;;	# OK
 	    p)	SPAGE="$(echo "$OPTARG" | tr '[:lower:]' '[:upper:]')";;	# OK
 	    i)	INTROTAG="$(gettagindex $SEFFECT $OPTARG $LOSEFFECT)"	;;
 	    w)	WTIME="$(gettagindex $SWTIME $OPTARG $LSWTIME)"		;;	# OK
 	    o)	OUTTROTAG="$(gettagindex $SEFFECT $OPTARG $LCSEFFECT)"	;;
-	    f)	SCOMMAND="resetsign"	;;	# OK
-	    t)	SCOMMAND="setsignclock"	;;	# OK
-	    d)	SERIALPORT="$OPTARG"	;;	# OK
 	    c)	SCOLORTAG="$(gettagindex $SCOLOR $OPTARG $LSCOLOR)"	;;
-	    k)	LINKPAGES="$(echo "$OPTARG" | tr '[:lower:]' '[:upper:]')"
-		SCOMMAND="linkpages"
-		;;	# OK
 	    l)	SLINE="$OPTARG"		;;	# OK
-	    R)	echo "-R $OPTARGS"	;;
+	    f)	FONTTAG="$(gettagindex $SFONT $OPTARG $LSFONT)"		;;
+	    t)	MYTEXT="$OPTARG"	;;
+	    R)	echo "-R $OPTARG"	;;
 	    h|\?|*)	usage		;;
 	esac
     done
@@ -221,10 +245,10 @@ parse_arguments ()
 process_arguments ()
 {
     if [ "$INTROTAG" = "1" ]; then taghelp -i $SEFFECT $LOSEFFECT; fi
-    if [ "$OUTTROTAG" = "1" ]; then taghelp -i $SEFFECT $LCSEFFECT; fi
-    if [ "$WTIME" = "1" ]; then taghelp -i $SWTIME $LSWTIME; fi
-    if [ "$SCOLORTAG" = "1" ]; then taghelp -i $SCOLOR $LSCOLOR; fi
-    if [ "$FONTTAG" = "1" ]; then taghelp -i $SFONT $LSFONT; fi
+    if [ "$OUTTROTAG" = "1" ]; then taghelp -o $SEFFECT $LCSEFFECT; fi
+    if [ "$WTIME" = "1" ]; then taghelp -w $SWTIME $LSWTIME; fi
+    if [ "$SCOLORTAG" = "1" ]; then taghelp -c $SCOLOR $LSCOLOR; fi
+    if [ "$FONTTAG" = "1" ]; then taghelp -f $SFONT $LSFONT; fi
     case "$SCOMMAND" in
 	linkpages)
 		preparedataforsign $(linkpages $LINKPAGES)
@@ -260,8 +284,6 @@ process_arguments ()
 parse_arguments $@
 process_arguments
 
-
-read -r
-preparedataforsign "$(makepage Q "$REPLY")"
+preparedataforsign "$(makepage Q $(textpage "$MYTEXT"))"
 
 #decimal to ascii:    printf "<W\\$(printf '%3o' $(( $1 + 65 )))>"
