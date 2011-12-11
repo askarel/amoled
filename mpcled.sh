de35f35fd62082f72f32e43ad0d716efe5dc59d0
Frederic <frederic@askarel.be>
2011-12-11 22:21:33 +0100

    mpcled.sh update the screen on changes only. This will cut down on the unpleasant flicker.

--- mpcled.sh
+++ mpcled.sh
@@ -13,27 +13,36 @@ echo "<KT>"|(bash ./amoled.sh -D $SPORT -A 1 -i immediate -o immediate -p $(echo
 
 while true; do
     MPC="$(mpc --host=$MPDHOST current|tr -s ' ' ' ')"
-    SPAGE=1
-    RADIO="$(echo $MPC | cut -d ':' -f 1)"
-    TRACK="$(echo $MPC | cut -d ':' -f 2-)"
-    # Break down 'radio' ID
-    RPAGE=1
-    while [ -n "$(echo $RADIO | cut -d '-' -f $RPAGE)" ]; do
-	SPAGE=$(( $SPAGE + 1 ))
-#	bash ./amoled.sh -D $SPORT -A 1 -i scrollleft -o scrollleft -w 2 -p $(echo $ALPHA|cut -c $SPAGE) -c red -t "$(echo $RADIO|cut -d '-' -f $RPAGE)"
-	echo "$(echo $RADIO|cut -d '-' -f $RPAGE |sed -e 's/ *$//g'|sed -e 's/^ *//g')" |(bash ./amoled.sh -D $SPORT -A 1 -i scrollleft -o scrollleft -w 2 -p $(echo $ALPHA|cut -c $SPAGE) -c red )
-	RPAGE=$(( $RPAGE + 1 ))
-    done
-    # Break down track ID
-    TPAGE=1
-    while [ -n "$(echo $TRACK | cut -d '-' -f $TPAGE)" ]; do
-	SPAGE=$(( $SPAGE + 1 ))
-	echo "$(echo $TRACK|cut -d '-' -f $TPAGE |sed -e 's/ *$//g'|sed -e 's/^ *//g')" |(bash ./amoled.sh -D $SPORT -A 1 -i scrollleft -o scrollleft -w 2 -p $(echo $ALPHA|cut -c $SPAGE) -c yellow )
-#	bash ./amoled.sh -D $SPORT -A 1 -i scrollleft -o scrollleft -w 2 -p $(echo $ALPHA|cut -c $SPAGE) -c yellow -t "$(echo $TRACK|cut -d '-' -f $TPAGE)"
-
-	TPAGE=$(( $TPAGE + 1 ))
-    done
-    bash ./amoled.sh -D $SPORT -K $(echo $ALPHA|cut -c 1-$SPAGE)
-    
-    sleep 20
+    if [ "$MPC" != "$OLDMPC" ]; then
+	SPAGE=1
+	RADIO="$(echo $MPC | cut -d ':' -f 1)"
+	TRACK="$(echo $MPC | cut -d ':' -f 2-)"
+	# Break down 'radio' ID
+	if [ "$RADIO" != "$OLDRADIO" ]; then
+	    RPAGE=1
+	    OLDRADIO="$RADIO"
+	    while [ -n "$(echo $RADIO | cut -d '-' -f $RPAGE)" ]; do
+		SPAGE=$(( $SPAGE + 1 ))
+#		bash ./amoled.sh -D $SPORT -A 1 -i scrollleft -o scrollleft -w 2 -p $(echo $ALPHA|cut -c $SPAGE) -c red -t "$(echo $RADIO|cut -d '-' -f $RPAGE)"
+		echo "$(echo $RADIO|cut -d '-' -f $RPAGE |sed -e 's/ *$//g'|sed -e 's/^ *//g')" |(bash ./amoled.sh -D $SPORT -A 1 -i scrollleft -o scrollleft -w 2 -p $(echo $ALPHA|cut -c $SPAGE) -c red )
+		RPAGE=$(( $RPAGE + 1 ))
+	    done
+	else
+	    SPAGE=$(( $SPAGE + $RPAGE -1 ))
+	fi
+	# Break down track ID
+	TPAGE=1
+	while [ -n "$(echo $TRACK | cut -d '-' -f $TPAGE)" ]; do
+	    SPAGE=$(( $SPAGE + 1 ))
+	    echo "$(echo $TRACK|cut -d '-' -f $TPAGE |sed -e 's/ *$//g'|sed -e 's/^ *//g')" |(bash ./amoled.sh -D $SPORT -A 1 -i scrollleft -o scrollleft -w 2 -p $(echo $ALPHA|cut -c $SPAGE) -c yellow )
+#		bash ./amoled.sh -D $SPORT -A 1 -i scrollleft -o scrollleft -w 2 -p $(echo $ALPHA|cut -c $SPAGE) -c yellow -t "$(echo $TRACK|cut -d '-' -f $TPAGE)"
+	    TPAGE=$(( $TPAGE + 1 ))
+	done
+	if [ "$SPAGE" != "$SPAGEOLD" ]; then
+	    bash ./amoled.sh -D $SPORT -K $(echo $ALPHA|cut -c 1-$SPAGE)
+	    SPAGEOLD=$SPAGE
+	fi
+	OLDMPC=$MPC
+    fi
+    sleep 5
 done
